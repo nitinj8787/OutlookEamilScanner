2af3b2c0c2c3d27ad64699fce970b4b54bbdb703
Nitin Jain <nitinj8787@gmail.com>
2020-06-03 09:19:25 +0100

    OAUth EWS & graph

---
+++ ScannerEWS1/Program.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.Exchange.WebServices.Data;
+using Microsoft.Identity.Client;
+
+namespace ScannerEWS1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hit 1 for EWS or 2 for EWS Delegate or 3  for EWS Limited Application permisison or 4 for Microsoft Graph");
+            var userInput = Console.ReadLine();
+
+            if (userInput == "1")
+                MainEWSAsync(args).Wait();
+            else if (userInput == "2")
+                MainEwsDelegateAsync(args).Wait();
+            else if (userInput == "3")
+                MainEWSLimitedAsync(args).Wait();
+            else if (userInput == "4")
+                MainGraphAsync(args).Wait();
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.WriteLine("Hit any key to exit...");
+                Console.ReadKey();
+            }
+        }
+
+        static async System.Threading.Tasks.Task MainEWSAsync(string[] args)
+        {
+            // Configure the MSAL client to get tokens
+            var ewsScopes = new string[] { "https://outlook.office.com/.default" };
+
+            var app = ConfidentialClientApplicationBuilder.Create(ConfigurationManager.AppSettings["appId"])
+                .WithAuthority(AzureCloudInstance.AzurePublic, ConfigurationManager.AppSettings["tenantId"])
+                .WithClientSecret(ConfigurationManager.AppSettings["clientSecret"])
+                .Build();
+
+            AuthenticationResult result = null;
+
+            try
+            {
+                // Make the interactive token request
+                result = await app.AcquireTokenForClient(ewsScopes)
+                    .ExecuteAsync();
+
+                // Configure the ExchangeService with the access token
+                var ewsClient = new ExchangeService();
+                ewsClient.Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx");
+                ewsClient.Credentials = new OAuthCredentials(result.AccessToken);
+
+                //Impersonate the mailbox you'd like to access.
+                ewsClient.ImpersonatedUserId = new ImpersonatedUserId(ConnectingIdType.SmtpAddress, "NitinJain@njaindev.onmicrosoft.com");
+
+                // Make an EWS call
+                var folders = ewsClient.FindFolders(WellKnownFolderName.MsgFolderRoot, new FolderView(10));
+                foreach (var folder in folders)
+                {
+                    Console.WriteLine($"Folder: {folder.DisplayName}");
+                }
+
+
+
+            }
+            catch (MsalException ex)
+            {
+                Console.WriteLine($"Error acquiring access token: {ex.ToString()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.ToString()}");
+            }
+        }
+
+        static async System.Threading.Tasks.Task MainEWSLimitedAsync(string[] args)
+        {
+            // Configure the MSAL client to get tokens
+            var ewsScopes = new string[] { "https://outlook.office.com/.default" };
+
+            var app = ConfidentialClientApplicationBuilder.Create(ConfigurationManager.AppSettings["appIdEwsLimitedApplication"])
+                .WithAuthority(AzureCloudInstance.AzurePublic, ConfigurationManager.AppSettings["tenantIdEwsLimitedApplication"])
+                .WithClientSecret(ConfigurationManager.AppSettings["clientSecretEwsLimitedApplication"])
+                .Build();
+
+            AuthenticationResult result = null;
+
+            try
+            {
+                // Make the interactive token request
+                result = await app.AcquireTokenForClient(ewsScopes)
+                    .ExecuteAsync();
+
+                // Configure the ExchangeService with the access token
+                var ewsClient = new ExchangeService();
+                ewsClient.Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx");
+                ewsClient.Credentials = new OAuthCredentials(result.AccessToken);
+
+                //Impersonate the mailbox you'd like to access.
+                //ewsClient.ImpersonatedUserId = new ImpersonatedUserId(ConnectingIdType.SmtpAddress, "NitinJain@njaindev.onmicrosoft.com");
+
+                // Make an EWS call
+                var folders = ewsClient.FindFolders(WellKnownFolderName.MsgFolderRoot, new FolderView(10));
+                foreach (var folder in folders)
+                {
+                    Console.WriteLine($"Folder: {folder.DisplayName}");
+                }
+
+
+
+            }
+            catch (MsalException ex)
+            {
+                Console.WriteLine($"Error acquiring access token: {ex.ToString()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.ToString()}");
+            }
+        }
+
+        static async System.Threading.Tasks.Task MainEwsDelegateAsync(string[] args)
+        {
+            // Configure the MSAL client to get tokens
+            var pcaOptions = new PublicClientApplicationOptions
+            {
+                ClientId = ConfigurationManager.AppSettings["appIdEwsDelegate"],
+                TenantId = ConfigurationManager.AppSettings["tenantIdEwsDelegate"]
+            };
+
+            var pca = PublicClientApplicationBuilder
+                .CreateWithApplicationOptions(pcaOptions).Build();
+
+            var ewsScopes = new string[] { "https://outlook.office.com/EWS.AccessAsUser.All" };
+
+            try
+            {
+                // Make the interactive token request
+                var authResult = await pca.AcquireTokenInteractive(ewsScopes).ExecuteAsync();
+
+                // Configure the ExchangeService with the access token
+                var ewsClient = new ExchangeService();
+                ewsClient.Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx");
+                ewsClient.Credentials = new OAuthCredentials(authResult.AccessToken);
+
+                // Make an EWS call
+                var folders = ewsClient.FindFolders(WellKnownFolderName.MsgFolderRoot, new FolderView(10));
+                foreach (var folder in folders)
+                {
+                    Console.WriteLine($"Folder: {folder.DisplayName}");
+                }
+            }
+            catch (MsalException ex)
+            {
+                Console.WriteLine($"Error acquiring access token: {ex.ToString()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.ToString()}");
+            }
+        }
+
+        static async System.Threading.Tasks.Task MainGraphAsync(string[] args)
+        {           
+
+            var clientId = ConfigurationManager.AppSettings["appIdGraph"];
+            var tenantId = ConfigurationManager.AppSettings["tenantIdGraph"];
+            var secretId = ConfigurationManager.AppSettings["secretIdGraph"];
+
+            //Set the API Endpoint to Graph 'me' endpoint
+            string graphAPIEndpoint = $"https://graph.microsoft.com/v1.0/me/messages/{clientId}";
+
+            //Set the scope for API call to user.read
+            string[] scopes = new string[] { "https://graph.microsoft.com/.default" };
+
+
+            IConfidentialClientApplication clientApp;
+            clientApp = ConfidentialClientApplicationBuilder.Create(clientId)
+                                                    .WithClientSecret(secretId)
+                                                    .WithAuthority(new Uri($"https://login.microsoftonline.com/{tenantId}"))
+                                                    .Build();
+
+            AuthenticationResult authResult = null;
+
+            try
+            {
+                //authResult = await clientApp.AcquireTokenSilent(scopes, firstAccount)
+                //    .ExecuteAsync();
+
+                authResult = await clientApp.AcquireTokenForClient(scopes)
+                   .ExecuteAsync();
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                // A MsalUiRequiredException happened on AcquireTokenSilent.
+                // This indicates you need to call AcquireTokenInteractive to acquire a token
+                Console.WriteLine($"MsalUiRequiredException: {ex.Message}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}");
+                return;
+            }
+
+            if (authResult != null)
+            {
+                var resultText = await GetHttpContentWithToken(graphAPIEndpoint, authResult.AccessToken);
+                Console.WriteLine(resultText);
+            }
+
+        }
+
+        /// <summary>
+        /// Perform an HTTP GET request to a URL using an HTTP Authorization header
+        /// </summary>
+        /// <param name="url">The URL</param>
+        /// <param name="token">The token</param>
+        /// <returns>String containing the results of the GET operation</returns>
+        static async Task<string> GetHttpContentWithToken(string url, string token)
+        {
+            var httpClient = new System.Net.Http.HttpClient();
+            System.Net.Http.HttpResponseMessage response;
+            try
+            {
+                var request = new System.Net.Http.HttpRequestMessage(System.Net.Http.HttpMethod.Get, url);
+                //Add the token in Authorization header
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                response = await httpClient.SendAsync(request);
+                var content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+    }
+}
+
